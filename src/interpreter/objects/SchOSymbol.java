8dfeac72fc9ca1523500ad668adfa08862865ca8
Philipp Toelke <toelke@in.tum.de>
2010-02-18 08:50:31 +0100

    For some reason java is out to get me;

--- src/interpreter/objects/SchOSymbol.java
+++ src/interpreter/objects/SchOSymbol.java
@@ -4,10 +4,11 @@ import java.util.HashMap;
 
 public class SchOSymbol extends SchObject {
     public String value;
-    private static HashMap<String, SchOSymbol> symbols = new HashMap<String,  SchOSymbol>();
+    private static HashMap<String, SchOSymbol> symbols;
 
     @SuppressWarnings({"StaticMethodOnlyUsedInOneClass"})
     public static SchOSymbol makeSymbol(String v) {
+        if (SchOSymbol.symbols == null) SchOSymbol.symbols = new HashMap<String, SchOSymbol>();
         if (SchOSymbol.symbols.containsKey(v))
             return SchOSymbol.symbols.get(v);
         return new SchOSymbol(v);
