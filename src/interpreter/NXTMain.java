e306141c76e3c1637c903d92b1b06a3e3dff6fd5
Philipp Toelke <philipptoelke@web.de>
2010-02-19 12:13:40 +0100

    implemented a main for the nxt

--- src/interpreter/NXTMain.java
+++ src/interpreter/NXTMain.java
@@ -1,11 +1,30 @@
 package interpreter;
 
+import interpreter.utils.Environment;
+import interpreter.utils.Utils;
+import java.io.InputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+import lejos.nxt.comm.NXTConnection;
+import lejos.nxt.comm.USB;
+
 @SuppressWarnings({"UtilityClass"})
 public class NXTMain {
     private NXTMain() {
     }
 
     public static void main(String... args) {
-        //FIXME
+		NXTConnection c = USB.waitForConnection();
+		InputStream in = c.openInputStream();
+		PrintStream out = new PrintStream(c.openOutputStream());
+		Repl r = new Repl(in, out);
+
+		try {
+			for(;;) {
+				out.print("> ");
+				r.write(r.eval(r.read(), Environment.the_global_environment));
+				out.print("\n");
+			}
+		} catch (IOException ignored) {}
     }
 }
