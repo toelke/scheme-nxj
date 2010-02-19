cddb60eb4c4c4a8741d414bc17b6c186a731b35f
Philipp Toelke <toelke@in.tum.de>
2010-02-19 11:59:40 +0100

    scaffolding for the NXTMain

---
+++ src/interpreter/NXTMain.java
@@ -0,0 +1,11 @@
+package interpreter;
+
+@SuppressWarnings({"UtilityClass"})
+public class NXTMain {
+    private NXTMain() {
+    }
+
+    public static void main(String... args) {
+        //FIXME
+    }
+}
